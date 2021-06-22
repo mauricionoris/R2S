289db8b44b1b6959ad2281a9f7077a19605223c6
vagrant <vagrant@R2S-Server>
2021-06-22 01:22:23 +0000

    TDD installation

---
+++ test/environment/recommender/exec_algorithm.js
@@ -0,0 +1,24 @@
+
+var assert = require('assert');
+describe('environment/recommender tests', function() {
+  describe('Executing a python Script', function() {
+    describe('Calling the script', function() {
+/*       it('should return -1 when the value is not present', function() {
+        assert.strictEqual([1, 2, 3].indexOf(4), -1);
+      }); */
+    });
+
+    describe('Viewing script log', function() {
+/*       it('should return -1 when the value is not present', function() {
+        assert.strictEqual([1, 2, 3].indexOf(4), -1);
+      }); */
+    });
+
+    describe('Checking the script output', function() {
+/*       it('should return -1 when the value is not present', function() {
+        assert.strictEqual([1, 2, 3].indexOf(4), -1);
+      }); */
+    });
+
+  });
+});
